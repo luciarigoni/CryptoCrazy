41cb5f6fa51a45e6de35f7bf1be73d594be12e20
luciarigoni <luciarigoni@live.com>
2022-11-09 14:42:18 -0300

    Final project using Swift UI

---
+++ CryptoCrazySwiftUI/CryptoCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySwiftUIApp.swift
+//  CryptoCrazySwiftUI
+//
+//  Created by Lucia Rigoni Freire on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
